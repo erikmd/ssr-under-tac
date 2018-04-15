8af7feba7d4964f7001b398c9ffa05505c148b23
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2018-04-15 16:20:42 +0200

    Fix compilation with Coq 8.7 (using MathComp 1.6.4)

--- src/under.v
+++ src/under.v
@@ -1,10 +1,6 @@
 (* This file was written by Erik Martin-Dorel, 2016 *)
 Require Import mathcomp.ssreflect.ssreflect.
-From mathcomp
 Require Import ssrmatching.
-
-(* Without this line, doesn't compile with Coq 8.5... (issue with ssrpattern) *)
-Declare ML Module "ssreflect".
 
 Set Implicit Arguments.
 Unset Strict Implicit.
